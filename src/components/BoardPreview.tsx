caed0a22c1674834b0ea75aff88bceeae4b5d0d7
Mykyta Neklesa <nikitaneklesa5@gmail.com>
2023-12-28 16:57:20 +0200

    add board tile comp for home page

---
+++ src/components/BoardPreview.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/BoardPreview.scss';
+import { BoardPreviewTile } from '../models/models';
+
+export default function BoardPreview({ title, custom }: BoardPreviewTile): JSX.Element {
+  return (
+    <div className="board__preview" style={custom}>
+      <h4 className="board__title">{title}</h4>
+    </div>
+  );
+}
