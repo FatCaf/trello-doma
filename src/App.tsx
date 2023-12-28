9c14bb29f174e4c13901ae2a4521ab115731710c
Mykyta Neklesa <nikitaneklesa5@gmail.com>
2023-12-28 17:04:44 +0200

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Board from './pages/Board';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
 
 export default function App(): JSX.Element {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/board/:boardId" element={<Board />} />
+        <Route path="/" element={<Home />} index />
+      </Routes>
+    </Router>
   );
 }
