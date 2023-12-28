79f30aaadd86e31b9f5f50994b4828e49700921e
Mykyta Neklesa <nikitaneklesa5@gmail.com>
2023-12-28 17:03:33 +0200

    minor changes + need fix ISettings issue

--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -1,50 +1,43 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router';
+import { AxiosError } from 'axios';
+import { useSelector } from 'react-redux';
 import BoardColumn from '../components/BoardColumn';
 import Settings from '../components/Settings';
 import '../styles/Board.scss';
 import { IBoard } from '../models/models';
 import settings from '../assets/settings.svg';
-
-const testBoardConf: IBoard = {
-  title: 'Моя тестова дошка',
-  lists: [
-    {
-      id: 1,
-      title: 'Плани',
-      cards: [
-        { id: 1, title: 'помити кота' },
-        { id: 2, title: 'приготувати суп' },
-        { id: 3, title: 'сходити в магазин' },
-      ],
-    },
-    {
-      id: 2,
-      title: 'В процесі',
-      cards: [{ id: 4, title: 'подивитися серіал' }],
-    },
-    {
-      id: 3,
-      title: 'Зроблено',
-      cards: [
-        { id: 5, title: 'зробити домашку' },
-        { id: 6, title: 'погуляти з собакой' },
-      ],
-    },
-  ],
-};
+import instance from '../api/requests';
+import { selectBodyColor } from '../store/slices/bodyColorSlice';
 
 export default function Board(): JSX.Element {
-  const [boardTitle, setBoardTitle] = useState('Board name');
-  const [bodyColor, setBodyColor] = useState('rgb(205, 90, 145)');
+  const bodyColor = useSelector(selectBodyColor);
+  const { boardId } = useParams<{ boardId: string }>();
+  const [board, setBoard] = useState<IBoard | null>(null);
+  const [boardTitle, setBoardTitle] = useState<string | undefined>('Board name');
   const [modal, setModal] = useState(false);
 
   useEffect(() => {
-    setBoardTitle(testBoardConf.title);
-  }, []);
+    async function fetchTest(): Promise<void> {
+      try {
+        const response: IBoard = await instance.get(`/board/${boardId}`);
+        setBoard(response);
+      } catch (e: unknown) {
+        const error = e as AxiosError;
+        throw new Error(error.message);
+      }
+    }
+
+    fetchTest();
+  }, [boardId]);
+
+  useEffect(() => {
+    setBoardTitle(board?.title);
+  }, [board?.title]);
 
   useEffect(() => {
     document.body.style.backgroundColor = bodyColor;
-  });
+  }, [bodyColor]);
 
   return (
     <div className="board" key="board">
@@ -55,16 +48,23 @@ export default function Board(): JSX.Element {
         </div>
       </div>
       <div className="columns">
-        {testBoardConf.lists.map((list) => (
-          <BoardColumn {...list} key={list.id} />
-        ))}
+        {board?.lists.map((list) => <BoardColumn {...list} key={list?.id} />)}
         <div className="add__column">
           <p>
-            <span>+</span> Додати ще одну колонку
+            <span>+</span> Додати нову колонку
           </p>
         </div>
       </div>
-      {modal && <Settings closeModal={() => setModal(false)} changeBodyColor={setBodyColor} />}
+      {modal && (
+        <Settings
+          closeModal={() => setModal(false)}
+          // eslint-disable-next-line func-names
+          // eslint-disable-next-line react/jsx-no-bind
+          onBoardAdded={function (): void {
+            throw new Error('Function not implemented.');
+          }}
+        />
+      )}
     </div>
   );
 }
