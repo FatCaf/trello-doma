b0dc80c86e12025fd47d8008c6f3a877f86c9c03
Mykyta Neklesa <nikitaneklesa5@gmail.com>
2024-01-03 15:47:57 +0200

    add delete func, redux state

--- src/components/ColumnCard.tsx
+++ src/components/ColumnCard.tsx
@@ -1,12 +1,57 @@
 import React from 'react';
-import { IColumnCard } from '../models/models';
+import { confirmAlert } from 'react-confirm-alert';
+import { useParams } from 'react-router';
+import { IColumnCard, IColumnCardDelete } from '../models/models';
 import '../styles/ColumnCard.scss';
+import { useAppDispatch, useAppSelector } from '../store/hooks';
+import { fetchBoard } from '../store/slices/boardSlice';
+import { deleteCard } from '../store/slices/cardSlice';
 
-export default function ColumnCard({ title }: IColumnCard): JSX.Element {
+export default function ColumnCard({ id, title }: IColumnCard): JSX.Element {
+  const dispatch = useAppDispatch();
+  const { boardId } = useParams<{ boardId: string }>();
+  const { status } = useAppSelector((state) => state.card);
+
+  const handleDelete = (event: React.MouseEvent<HTMLDivElement>): void => {
+    const name = event.currentTarget.getAttribute('data-name');
+    const cardId = event.currentTarget.getAttribute('id');
+    const deleteData: IColumnCardDelete = {
+      boardId,
+      cardId,
+    };
+
+    confirmAlert({
+      title: 'Видалити картку',
+      message: `Чи точно ви хочете видалити картку ${name}`,
+      buttons: [
+        {
+          label: 'Так',
+          onClick: async (): Promise<void> => {
+            try {
+              await dispatch(deleteCard(deleteData));
+              if (status === 'resolved') await dispatch(fetchBoard(boardId));
+            } catch (e: unknown) {
+              const error = e as string;
+              throw new Error(error);
+            }
+          },
+        },
+        {
+          label: 'Ні',
+          // eslint-disable-next-line no-console
+          onClick: () => console.log('no delete'),
+        },
+      ],
+    });
+  };
+
   return (
     <div className="card">
       <div className="card__title">
         <h4>{title}</h4>
+      </div>
+      <div className="card__delete" id={`${id}`} data-name={title} onClick={handleDelete}>
+        <span>X</span>
       </div>
     </div>
   );
