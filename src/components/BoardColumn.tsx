6e191930f12bf1fee1a26641ff29857b33e0624c
Mykyta Neklesa <nikitaneklesa5@gmail.com>
2024-01-06 20:26:20 +0200

    add inputs state

--- src/components/BoardColumn.tsx
+++ src/components/BoardColumn.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { useParams } from 'react-router';
 import { confirmAlert } from 'react-confirm-alert';
 import ColumnCard from './ColumnCard';
@@ -7,15 +7,17 @@ import { IBoardColumn, IColumnCardPost, IColumnDelete, IColumnEdit } from '../mo
 import { addCard } from '../store/slices/cardSlice';
 import { useAppDispatch, useAppSelector } from '../store/hooks';
 import { deleteColumn, editColumn, fetchBoard } from '../store/slices/boardSlice';
-import { openInput } from '../store/slices/inputSlice';
+import { closeInput, openInput } from '../store/slices/inputSlice';
+import useOutsideClick from '../hooks/useOutsideClick';
 
 export default function BoardColumn({ id, title, cards }: IBoardColumn): JSX.Element {
   const { boardId } = useParams<{ boardId: string }>();
   const dispatch = useAppDispatch();
-  const { status } = useAppSelector((state) => state.card);
   const inputs = useAppSelector((state) => state.input.inputs);
   const [cardTitle, setCardTitle] = useState('Безіменна картка');
   const [columnTitle, setColumnTitle] = useState('Безіменна колонка');
+  const cardRef = useRef<HTMLDivElement>(null);
+  const columnRef = useRef<HTMLDivElement>(null);
 
   const handleAdd = async (event: React.MouseEvent<HTMLFormElement>): Promise<void> => {
     const listId = +event.currentTarget.id;
@@ -26,7 +28,7 @@ export default function BoardColumn({ id, title, cards }: IBoardColumn): JSX.Ele
         data: {
           title: cardTitle,
           list_id: listId,
-          position: 1,
+          position: cards?.length ? cards.length + 1 : 1,
           description: '',
           custom: {
             deadline: '',
@@ -34,7 +36,7 @@ export default function BoardColumn({ id, title, cards }: IBoardColumn): JSX.Ele
         },
       };
       await dispatch(addCard(postData));
-      if (status === 'resolved') await dispatch(fetchBoard(boardId));
+      await dispatch(fetchBoard(boardId));
     } catch (e: unknown) {
       const error = e as string;
       throw new Error(error);
@@ -51,14 +53,14 @@ export default function BoardColumn({ id, title, cards }: IBoardColumn): JSX.Ele
 
     confirmAlert({
       title: 'Видалити колонку',
-      message: `Чи точно ви хочете видалити колонку ${name}`,
+      message: `Чи точно ви хочете видалити колонку ${name} ?`,
       buttons: [
         {
           label: 'Так',
           onClick: async (): Promise<void> => {
             try {
               await dispatch(deleteColumn(deleteData));
-              if (status === 'resolved') await dispatch(fetchBoard(boardId));
+              await dispatch(fetchBoard(boardId));
             } catch (e: unknown) {
               const error = e as string;
               throw new Error(error);
@@ -87,35 +89,45 @@ export default function BoardColumn({ id, title, cards }: IBoardColumn): JSX.Ele
         },
       };
       await dispatch(editColumn(editData));
-      if (status === 'resolved') await dispatch(fetchBoard(boardId));
+      await dispatch(fetchBoard(boardId));
     } catch (e: unknown) {
       const error = e as string;
       throw new Error(error);
     }
   };
 
-  const handleClick = (inputId: string): void => {
+  useOutsideClick(columnRef.current, () => {
+    dispatch(closeInput());
+  });
+
+  useOutsideClick(cardRef.current, () => {
+    dispatch(closeInput());
+  });
+
+  const handleClick = (inputId: string, event: React.MouseEvent<HTMLDivElement, MouseEvent>): void => {
+    event.stopPropagation();
     dispatch(openInput({ id: inputId }));
   };
 
   return (
-    <div className="column">
+    <div className="column" key={id}>
       <div className="column__header">
-        <div className="column__title">
-          {inputs[`${id}`] ? (
-            <form id={`${id}`} onSubmit={handleEdit}>
+        <div className="column__title" id={`${id}`} onClick={(event) => handleClick(event.currentTarget.id, event)}>
+          {inputs[0]?.id === `${id}` ? (
+            <form id={`${id}`} onSubmit={handleEdit} className="input__form">
               <input
+                className="form__input"
                 type="text"
                 placeholder="Введіть назву колонки"
                 required
                 onChange={(event) => setColumnTitle(event?.target.value)}
               />
-              <button type="submit">Змінити</button>
+              <button className="add" type="submit">
+                Змінити
+              </button>
             </form>
           ) : (
-            <h4 id={`${id}`} onClick={(event) => handleClick(event.currentTarget.id)}>
-              {title}
-            </h4>
+            <h4>{title}</h4>
           )}
         </div>
         <div className="column__delete" id={`${id}`} data-name={title} onClick={handleDelete}>
@@ -126,18 +138,22 @@ export default function BoardColumn({ id, title, cards }: IBoardColumn): JSX.Ele
         {cards?.map((card) => <ColumnCard {...card} key={card.id} />)}
         <div
           className="add__card"
+          ref={cardRef}
           id={`${typeof id === 'number' ? id + 1 : id}`}
-          onClick={(event) => handleClick(event.currentTarget.id)}
+          onClick={(event) => handleClick(event.currentTarget.id, event)}
         >
-          {inputs[`${typeof id === 'number' ? id + 1 : id}`] ? (
-            <form id={`${id}`} onSubmit={handleAdd}>
+          {inputs[0]?.id === `${typeof id === 'number' ? id + 1 : id}` ? (
+            <form id={`${id}`} onSubmit={handleAdd} className="input__form">
               <input
+                className="form__input"
                 type="text"
                 placeholder="Введіть назву картки"
                 required
                 onChange={(event) => setCardTitle(event?.target.value)}
               />
-              <button type="submit">Додати</button>
+              <button className="add" type="submit">
+                Додати
+              </button>
             </form>
           ) : (
             <p>
