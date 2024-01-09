f297adf61a28a557c2f08b6186e71384cbc44e49
Mykyta Neklesa <nikitaneklesa5@gmail.com>
2024-01-09 15:38:00 +0200

    fix 404 issue while deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import react from '@vitejs/plugin-react';
 dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react()],
   define: {
     'process.env': {},
