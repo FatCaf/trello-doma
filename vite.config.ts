479e9f33a778da5445bcd9fd539b764da6f396f4
Mykyta Neklesa <nikitaneklesa5@gmail.com>
2024-03-21 19:08:24 +0200

    remove base

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,14 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import dotenv from 'dotenv';
 import react from '@vitejs/plugin-react';
 
-function removeCrossorigin(): any {
-  return {
-    name: 'remove-module',
-    transformIndexHtml(html: string): any {
-      return html.replace('crossorigin', '').replace('type="module"', 'defer');
-    },
-  };
-}
-
 dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), removeCrossorigin()],
+  plugins: [react()],
   define: {
     'process.env': {},
     __VALUE__: `${process.env.VITE__VALUE}`,
   },
-  base: './',
 });
